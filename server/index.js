eb9910e45b4adfdfe402bfc3abca7e904a7aee17
Rafael Espinoza <nerddiffer@gmail.com>
2016-09-29 13:13:19 -0700

    Add logger & body parser middleware for server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
+
+/* Middleware */
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const pathToStaticDir = path.resolve(__dirname, '..', 'client');
 app.use(express.static(pathToStaticDir));
 
 /* Routes */
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.status(200).send();
 });
 
-var port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port);
 console.log('Server is listening on port', port);
